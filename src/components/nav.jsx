cd4ef68eae4629046f59663a9c6276855c553a76
luisfelipe-tec <luisfelipe.tecnologias@gmail.com>
2023-08-30 20:34:58 -0300

    refatorando nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -6,25 +6,26 @@ import { AiOutlineHeart } from "react-icons/ai";
 import { IoPersonOutline } from "react-icons/io5";
 
 const Nav = () => {
-  return;
-  <nav className="navigation-menu">
-    <a href="#">
-      <AiFillHome size={19} />
-      <span>Home</span>
-    </a>
-    <a href="#">
-      <BiTask size={19} />
-      <span>Plantões</span>
-    </a>
-    <a href="#">
-      <AiOutlineHeart size={19} />
-      <span>Favoritos</span>
-    </a>
-    <a href="#">
-      <IoPersonOutline size={19} />
-      <span>Minha Conta</span>
-    </a>
-  </nav>;
+  return (
+    <nav className="navigation-menu">
+      <a href="#">
+        <AiFillHome size={19} />
+        <span>Home</span>
+      </a>
+      <a href="#">
+        <BiTask size={19} />
+        <span>Plantões</span>
+      </a>
+      <a href="#">
+        <AiOutlineHeart size={19} />
+        <span>Favoritos</span>
+      </a>
+      <a href="#">
+        <IoPersonOutline size={19} />
+        <span>Minha Conta</span>
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
